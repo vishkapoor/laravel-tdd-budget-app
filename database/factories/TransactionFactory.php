fc1faff621ee7cf60b2ce23c8c8bbf6a369e1421
Vishal Kapoor <vishal.kapoor@ecdltd.co.uk>
2019-12-29 12:58:13 +0000

    wip

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Models\Transaction;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    return [
+        'description' => $faker->sentence(10),
+    ];
+});
